9a3f3eb8a7a64f3b5ed615c4013c3c46c762a9a6
Geoffrey Huntley <ghuntley@ghuntley.com>
2025-05-10 11:46:21 +0000

    dsadsa

--- context_fix.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Extract the LlvmCodeGenerator struct and implementation
-grep -v '// Import of other module implementations is done in respective files' src/codegen/llvm/context.rs > fixed_context.rs
-# Move the fixed file over the original
-mv fixed_context.rs src/codegen/llvm/context.rs
--- fix_tokens.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Find all lines with 'token: Token::new(' and fix them by adding '.token_literal()'
-sed -i 's/token: Token::new(\(.*\)),/token: Token::new(\1).token_literal(),/g' tests/struct_field_type_inference_test.rs
